c9dea542eab7ed92ae7e26f3af2d976aee0d344d
mako4kamiya <mako4kamiya@gmail.com>
2020-08-01 14:07:09 +0900

    2_Node.js 学習コース I 終了

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/')
 
-router.get('/', function (req, res, next) {
-  controllers.showIndexPage(req,res);
-})
+// router.get('/', function (req, res, next) {
+//   controllers.showIndexPage(req,res);
+// })
 
 router.get('/users', function (req, res, next) {
   controllers.users.showUsers(req,res);
 })
+
+router.get('/', (req, res) => {
+  res.render('top');
+});
+
+router.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
 
 module.exports = router;
