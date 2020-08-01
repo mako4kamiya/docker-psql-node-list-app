fe48535d01f1583a38a74de8c587ace65c1fb3e7
mako4kamiya <mako4kamiya@gmail.com>
2020-08-02 02:25:16 +0900

    メモ一覧を昇順に

--- models/queries/items.js
+++ models/queries/items.js
@@ -1,8 +1,7 @@
 const knex = require('../knex');
 
-exports.getItems = () => knex.from('items').select("*")
-exports.createItems = () => knex.from('items').select("*")
+exports.getItems = () => knex.select().from('items').orderBy('id', 'ASC')
 exports.createItems = (itemName) => knex("items").insert({name: itemName})
 exports.deleteItems = (itemId) => knex("items").where('id', itemId).del()
-exports.getThisItem = (itemId) => knex.from('items').where('id', itemId).select("*")
+exports.getThisItem = (itemId) => knex.select().from('items').where('id', itemId)
 exports.updateItem = (itemId,itemName) => knex("items").where('id', itemId).update({name: itemName})
